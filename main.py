3b1d4a54ba6b27e9753bee131356c35f1a033712
Arkady Shenberg <shenbergarkadii@gmail.com>
2021-12-03 23:41:00 +0700

    Added func 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools
+from enum import Enum
 
 
 def is_full_connected(apexes, edges, links):
@@ -18,6 +19,49 @@ def is_full_connected(apexes, edges, links):
         return True
     else:
         return False
+
+
+def connection_type(apexes, edges, links):
+    """
+    Функция, определяющая тип неполносвязной сети.
+    1 - шина, 2 - кольцо, 3 - звезда.
+    """
+
+    class ConnectionType(Enum):
+        """
+        Enum перечисление возможных вариантов.
+        """
+        tire = 1
+        ring = 2
+        star = 3
+
+    if apexes == edges:
+        return ConnectionType.ring.value
+    elif apexes == edges + 1:
+        is_star = False
+        for i in range(apexes):
+            flag = 0
+            for j in range(len(links)):
+                if i in links[j]:
+                    flag += 1
+                else:
+                    flag = 0
+                    break
+            if flag == edges:
+                is_star = True
+                break
+        if is_star is True:
+            return ConnectionType.star.value
+        else:
+            t = 0
+            for i in range(1, apexes):
+                if (i - 1, i) in links:
+                    t += 1
+                else:
+                    t = 0
+                    break
+            if t == edges:
+                return ConnectionType.tire.value
 
 
 CLI = argparse.ArgumentParser(description='Данная программа умеет определять '
@@ -55,5 +99,14 @@ args.links = list(map(lambda x: tuple(sorted(x)), args.links))
 if is_full_connected(args.v, args.r, args.links):
     print('Сеть полносвязная.')
     quit()
-# else:
-        
+else:
+    temp = connection_type(args.v, args.r, args.links)
+    if temp == 1:
+        print('Неполносвязная сеть типа "шина"')
+        quit()
+    if temp == 2:
+        print('Неполносвязная сеть типа "кольцо"')
+        quit()
+    if temp == 3:
+        print('Неполносвязная сеть типа "звезда"')
+        quit()
